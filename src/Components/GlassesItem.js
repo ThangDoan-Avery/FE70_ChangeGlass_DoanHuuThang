c12a47d7581107414ab78851a25fdc0a61159b6b
unknown <thangdoanplus@gmail.com>
2022-03-05 23:21:33 +0700

    Update file

---
+++ src/Components/GlassesItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class GlassesItem extends Component {
+  render() {
+    return <div>GlassesItem</div>;
+  }
+}
---
+++ src/Components/GlassesList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import GlassesItem from './GlassesItem';
+
+export default class GlassesList extends Component {
+  render() {
+    return (
+      <div>
+        GlassesList
+        <GlassesItem />
+      </div>
+    );
+  }
+}
